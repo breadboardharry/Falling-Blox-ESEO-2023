f4657b034c3a363b49c40f7f70566f4a49cde3de
HarryOnBreadboard <a.leroux330@laposte.net>
2023-04-12 11:15:05 +0200

    3.2.4 Créer un Element

---
+++ src/fr/eseo/e3/poo/projet/blox/modele/Element.java
@@ -0,0 +1,60 @@
+package fr.eseo.e3.poo.projet.blox.modele;
+
+import java.util.Objects;
+
+public class Element {
+
+    private Couleur couleur;
+    private Coordonnees coordonnees;
+
+    public Element(Coordonnees coordonnees) {
+        this(coordonnees, Couleur.values()[0]);
+    }
+
+    public Element(int abscisse, int ordonnee) {
+        this(new Coordonnees(abscisse, ordonnee));
+    }
+
+    public Element(Coordonnees coordonnees, Couleur couleur) {
+        this.coordonnees = coordonnees;
+        this.couleur = couleur;
+    }
+
+    public Element(int abscisse, int ordonnee, Couleur couleur) {
+        this(new Coordonnees(abscisse, ordonnee), couleur);
+    }
+
+    public Coordonnees getCoordonnees() {
+        return this.coordonnees;
+    }
+
+    public void setCoordonnees(Coordonnees coordonnees) {
+        this.coordonnees = coordonnees;
+    }
+
+    public Couleur getCouleur() {
+        return this.couleur;
+    }
+
+    public void setCouleur(Couleur couleur) {
+        this.couleur = couleur;
+    }
+
+    @Override
+    public String toString() {
+        return this.coordonnees.toString() + " - " + this.couleur.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Element element = (Element) o;
+        return this.couleur == element.couleur && Objects.equals(this.coordonnees, element.coordonnees);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.coordonnees, this.couleur);
+    }
+}
