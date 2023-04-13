deb3515cd71b3e54d5fc4988bcfa85df3b535e5f
HarryOnBreadboard <a.leroux330@laposte.net>
2023-04-13 16:02:03 +0200

    3.2.9 Vérifier les classes IPiece et OPiece

--- test/fr/eseo/e3/poo/projet/blox/modele/pieces/IPieceTest.java
+++ test/fr/eseo/e3/poo/projet/blox/modele/pieces/IPieceTest.java
@@ -7,40 +7,40 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OPieceTest {
+public class IPieceTest {
 
     @Test
     void testConstructeur() {
-        OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
+        IPiece piece = new IPiece(new Coordonnees(0, 0), Couleur.BLEU);
         assertEquals(4, piece.getElements().size(), "Vérifier nombre d'éléments");
     }
 
     @Test
     void testGetElements() {
-        OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
+        IPiece piece = new IPiece(new Coordonnees(0, 0), Couleur.BLEU);
         assertEquals(new Element(0, 0, Couleur.BLEU), piece.getElements().get(0), "Vérifier élément 1");
-        assertEquals(new Element(1, 0, Couleur.BLEU), piece.getElements().get(1), "Vérifier élément 1");
+        assertEquals(new Element(0, -2, Couleur.BLEU), piece.getElements().get(1), "Vérifier élément 1");
         assertEquals(new Element(0, -1, Couleur.BLEU), piece.getElements().get(2), "Vérifier élément 1");
-        assertEquals(new Element(1, -1, Couleur.BLEU), piece.getElements().get(3), "Vérifier élément 1");
+        assertEquals(new Element(0, 1, Couleur.BLEU), piece.getElements().get(3), "Vérifier élément 1");
     }
 
     @Test
     void testSetPosition() {
-        OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
+        IPiece piece = new IPiece(new Coordonnees(0, 0), Couleur.BLEU);
         piece.setPosition(1, 1);
         assertEquals(new Element(1, 1, Couleur.BLEU), piece.getElements().get(0), "Vérifier élément 1");
-        assertEquals(new Element(2, 1, Couleur.BLEU), piece.getElements().get(1), "Vérifier élément 1");
+        assertEquals(new Element(1, -1, Couleur.BLEU), piece.getElements().get(1), "Vérifier élément 1");
         assertEquals(new Element(1, 0, Couleur.BLEU), piece.getElements().get(2), "Vérifier élément 1");
-        assertEquals(new Element(2, 0, Couleur.BLEU), piece.getElements().get(3), "Vérifier élément 1");
+        assertEquals(new Element(1, 2, Couleur.BLEU), piece.getElements().get(3), "Vérifier élément 1");
     }
 
     @Test
     void testToString() {
-        OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
-        assertEquals("OPiece :\n" +
+        IPiece piece = new IPiece(new Coordonnees(0, 0), Couleur.BLEU);
+        assertEquals("IPiece :\n" +
                 "\t(0, 0) - BLEU\n" +
-                "\t(1, 0) - BLEU\n" +
+                "\t(0, -2) - BLEU\n" +
                 "\t(0, -1) - BLEU\n" +
-                "\t(1, -1) - BLEU\n", piece.toString(), "Vérifier toString");
+                "\t(0, 1) - BLEU\n", piece.toString(), "Vérifier toString");
     }
 }
