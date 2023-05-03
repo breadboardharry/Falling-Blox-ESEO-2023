95205373b01c0a13b4b3e6212aea3d4ae9f3eeec
HarryOnBreadboard <a.leroux330@laposte.net>
2023-05-03 10:35:13 +0200

    3.3.3 Dessiner sur la VuePuits

--- src/fr/eseo/e3/poo/projet/blox/vue/VuePuits.java
+++ src/fr/eseo/e3/poo/projet/blox/vue/VuePuits.java
@@ -3,6 +3,7 @@ package fr.eseo.e3.poo.projet.blox.vue;
 import fr.eseo.e3.poo.projet.blox.modele.Puits;
 
 import javax.swing.JPanel;
+import java.awt.*;
 
 public class VuePuits extends JPanel {
 
@@ -26,6 +27,7 @@ public class VuePuits extends JPanel {
 
     public void setPuits(Puits puits) {
         this.puits = puits;
+        setPreferredSize(new Dimension(this.puits.getLargeur() * this.taille, this.puits.getProfondeur() * this.taille));
     }
 
     public int getTaille() {
@@ -34,5 +36,25 @@ public class VuePuits extends JPanel {
 
     public void setTaille(int taille) {
         this.taille = taille;
+        setPreferredSize(new Dimension(this.puits.getLargeur() * this.taille, this.puits.getProfondeur() * this.taille));
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g.create();
+
+        // Set background color as white
+        g2D.setColor(java.awt.Color.WHITE);
+        g2D.fillRect(0, 0, getWidth(), getHeight());
+
+        // Draw grid
+        g2D.setColor(java.awt.Color.LIGHT_GRAY);
+        for (int x = 0; x < getWidth(); x += this.taille) {
+            for (int y = 0; y < getHeight(); y += this.taille) {
+                g2D.drawRect(x, y, this.taille, this.taille);
+            }
+        }
+
+        g2D.dispose();
     }
 }
