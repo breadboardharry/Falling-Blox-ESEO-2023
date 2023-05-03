b66e090bc0566dd0d4551ae0c55c6b15b9b1f5da
HarryOnBreadboard <a.leroux330@laposte.net>
2023-05-03 10:51:13 +0200

    3.3.5 Modifier l’énumération Couleur

--- src/fr/eseo/e3/poo/projet/blox/modele/Couleur.java
+++ src/fr/eseo/e3/poo/projet/blox/modele/Couleur.java
@@ -1,11 +1,23 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
+import java.awt.Color;
+
+import static java.awt.Color.*;
+
 public enum Couleur {
-    ROUGE,
-    ORANGE,
-    BLEU,
-    VERT,
-    JAUNE,
-    CYAN,
-    VIOLET;
+    ROUGE(java.awt.Color.RED),
+    ORANGE(java.awt.Color.ORANGE),
+    BLEU(java.awt.Color.BLUE),
+    VERT(java.awt.Color.GREEN),
+    JAUNE(java.awt.Color.YELLOW),
+    CYAN(java.awt.Color.CYAN),
+    VIOLET(java.awt.Color.MAGENTA);
+
+    private final Color couleurPourAffichage;
+    Couleur(Color couleur) {
+        this.couleurPourAffichage = couleur;
+    }
+    public Color getCouleurPourAffichage() {
+        return this.couleurPourAffichage;
+    }
 }
