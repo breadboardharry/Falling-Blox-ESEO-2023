077482cc857edca58616db99a6a6fd0d3c4d3ce1
HarryOnBreadboard <a.leroux330@laposte.net>
2023-05-12 15:44:40 +0200

    3.5.6 Vérifier la rotation avec la souris

--- test/fr/eseo/e3/poo/projet/blox/controleur/PieceRotationTest.java
+++ test/fr/eseo/e3/poo/projet/blox/controleur/PieceRotationTest.java
@@ -8,9 +8,10 @@ import fr.eseo.e3.poo.projet.blox.vue.VuePuits;
 import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
 
-public class PieceDeplacementTest {
-    public PieceDeplacementTest() {
-        testPieceDeplacement();
+public class PieceRotationTest {
+
+    public PieceRotationTest() {
+        testPieceRotation();
     }
 
     // Run test thread
@@ -18,27 +19,29 @@ public class PieceDeplacementTest {
         SwingUtilities.invokeLater(new Runnable () {
             @Override
             public void run() {
-                new PieceDeplacementTest();
+                new PieceRotationTest();
             }
         });
     }
 
-    private void testPieceDeplacement() {
+    private void testPieceRotation() {
         // Create puits
         Puits puits = new Puits();
-        VuePuits vuePuits = new VuePuits(puits, 25);
+        VuePuits vuePuits = new VuePuits(puits);
 
         // Generate pieces
         UsineDePiece.setMode(UsineDePiece.CYCLIC);
         Piece piece1 = UsineDePiece.genererPiece();
         Piece piece2 = UsineDePiece.genererPiece();
+        Piece piece3 = UsineDePiece.genererPiece();
 
         // Set the piece to display
         puits.setPieceSuivante(piece1);
         puits.setPieceSuivante(piece2);
+        puits.setPieceSuivante(piece3);
 
         // Create frame
-        JFrame frame = new JFrame("Puits et taille");
+        JFrame frame = new JFrame("Test rotation piece");
         frame.setSize(puits.getLargeur() * vuePuits.getTaille(), puits.getProfondeur() * vuePuits.getTaille());
         frame.setResizable(false); // Disable resize
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
